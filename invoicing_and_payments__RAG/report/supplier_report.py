c0758c9ae96ff2a5bf09fbc2aeefd1522887b4d8
Akhilesh <akhilesh.gupta@apagen.com>
2015-05-19 16:50:03 +0530

    [Add changes]

--- invoicing_and_payments__RAG/report/supplier_report.py
+++
@@ -1,9 +0,0 @@
-from openerp.report import report_sxw
-from datetime import datetime
-import openerp.tools
-
-class Parser(report_sxw.rml_parse):
-    def __init__(self, cr, uid, name, context=None):
-        super(Parser, self).__init__(cr, uid, name, context=context)
-        self.localcontext.update({
-        })
